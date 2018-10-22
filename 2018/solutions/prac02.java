e791cdcd8be5a60e9476d1b1bc0f1ae3755351b8
Jason Klein <jason.klein@logicforte.com>
2018-10-21 22:54:18 -0500

    2018 Practice Packet, Inputs, Solutions

---
+++ 2018/solutions/prac02.java
@@ -0,0 +1,17 @@
+import java.io.*;
+ 
+public class prac02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader
+            = new BufferedReader(new InputStreamReader(System.in));
+ 
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            String words[] = line.trim().split("\\s+");
+            String noun = words[0];
+            String verb = words[1];
+            System.out.println(noun + " is " + verb + " today!");
+        }
+    }
+}
+
