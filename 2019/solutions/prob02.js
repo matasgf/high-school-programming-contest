ce200d0659172141277dbb3d6e5aae72458334a1
Tom Rankin <tom@tomrankin.net>
2019-10-21 20:06:27 -0500

    Prob 2 JS

--- 2019/solutions/prob02.js
+++ 2019/solutions/prob02.js
@@ -7,9 +7,10 @@ const readInterface = readline.createInterface({
 
 readInterface.on('line', function(line) {
     numbers = line.split(' ');
-    let unique = [...new Set(numbers)].map((val, index) => {
+    let unique = [...new Set(numbers)];
+    unique = unique.map((val, index) => {
         return val * 1;
-});
-    unique.sort((a, b) => {return a-b});
-    console.log(unique.join(' '));
+    });
+    unique.sort((a, b) => {return b-a});
+    console.log(unique.slice(0, 3).join(' '));
 });
