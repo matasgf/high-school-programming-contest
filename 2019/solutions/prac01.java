d7a145b2241ffe1b68cf4ed8af5af55d4c8d5dd0
Jason Klein <jason.klein@logicforte.com>
2019-11-09 10:02:48 -0600

    Prepare to merge private 2019 problem/solution repo

--- 2019/solutions/prac01.java
+++
@@ -1,7 +0,0 @@
-import java.io.*;
-
-public class prac01 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
--- 2019/solutions/prac02.py3
+++
@@ -1,5 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    noun, verb = line.split()
-    print(noun, "is", verb, "today!")
